f38b529001781aaf02f8fd86503aa073614bda7f
tomer-van-cohen <40365314+tomer-van-cohen@users.noreply.github.com>
2023-09-05 11:47:48 +0700

    Update xss-example.js

--- xss-example.js
+++ xss-example.js
@@ -2,4 +2,4 @@ var urlParams = new URLSearchParams(window.location.search);
 var name = urlParams.get('name');
 
 var unsafe_div = window.document.getElementById("vulnerable-div");
-unsafe_div.innerText = "Hello " + name;
+unsafe_div.innerHTML = "Hello " + name;
